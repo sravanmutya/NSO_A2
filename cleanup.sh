0496ba0f6acbbdaba8c26545da943413b38b87c9
sravanmutya <sravanmuty@gmail.com>
2023-06-14 10:48:24 +0530

    Stored input arguments to variables
    The input arguments of openrc, tag, and ssh_key were stored into variables.

--- cleanup.sh
+++ cleanup.sh
@@ -4,3 +4,9 @@
 : ${1:?" Please specify openrc. "}
 : ${2:?" Please specify the tag. "}
 : ${3:?" Please specify the ssh_key. "}
+
+# Taking input arguments and storing them into variables
+# Following naming convention of using p at the end of the variable meaning for project.
+openrc_p=${1}
+tag_p=${2}
+ssh_key_p=${3}
