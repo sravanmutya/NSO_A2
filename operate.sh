bbabbf6e969b37606055889afe8c926d8ed91c1b
sravanmutya <sravanmuty@gmail.com>
2023-06-14 10:36:31 +0530

    Added shebang
    Forgot to add shebang, added now

--- operate.sh
+++ operate.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Checking if the required arguments are passed by the user - the openrc, the tag and the ssh_key
 : ${1:?" Please specify openrc. "}
 : ${2:?" Please specify the tag. "}
