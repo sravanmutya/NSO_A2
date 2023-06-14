5bc3d6b07683c5cea4c114f1c78b37cddd9045da
sravanmutya <sravanmuty@gmail.com>
2023-06-14 10:33:50 +0530

    Created operate.sh

---
+++ operate.sh
@@ -0,0 +1,4 @@
+# Checking if the required arguments are passed by the user - the openrc, the tag and the ssh_key
+: ${1:?" Please specify openrc. "}
+: ${2:?" Please specify the tag. "}
+: ${3:?" Please specify the ssh_key. "}
